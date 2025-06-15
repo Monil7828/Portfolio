5daabdaee37c43462215549cab530d4aa8be5345
Monil7828 <monilc2801@gmail.com>
2025-06-15 12:36:53 +0530

    Dp fix 2

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -1,6 +1,7 @@
+// src/components/FrameWorks.jsx
 import { OrbitingCircles } from "./OrbitingCircles";
 
-export function Frameworks() {
+function FrameWorks() {
   const skills = [
     "auth0",
     "blazor",
@@ -19,22 +20,29 @@ export function Frameworks() {
     "vitejs",
     "next",
   ];
+
   return (
     <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
       <OrbitingCircles iconSize={40}>
         {skills.map((skill, index) => (
-          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+          <Icon key={index} src={`/assets/logos/${skill}.svg`} />
         ))}
       </OrbitingCircles>
       <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
-        {skills.reverse().map((skill, index) => (
-          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        {[...skills].reverse().map((skill, index) => (
+          <Icon key={index} src={`/assets/logos/${skill}.svg`} />
         ))}
       </OrbitingCircles>
     </div>
   );
 }
-export default Frameworks;
+
 const Icon = ({ src }) => (
-  <img src={src} className="duration-200 rounded-sm hover:scale-110" />
+  <img 
+    src={src} 
+    className="duration-200 rounded-sm hover:scale-110" 
+    alt="Skill icon"
+  />
 );
+
+export default FrameWorks;
