ae773985bf0bd0d8b08a81b50ec1b2172f458bf0
Monil7828 <monilc2801@gmail.com>
2025-06-15 13:42:51 +0530

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "./sections/navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
@@ -7,6 +6,7 @@ import Experiences from "./sections/Experiences";
 import Testimonial from "./sections/Testimonial";
 import Contact from "./sections/Contact";
 import Footer from './sections/Footer';
+import Navbar from "./sections/navbar";
 
 const App = () => {
   return (
